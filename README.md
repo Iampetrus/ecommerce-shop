28be2c170a8f6bbe4cd458711acfdab8e6b3fcff
Iampetrus <pedroalerri@yahoo.es>
2024-09-26 10:40:47 +0200

    Filtering product categories, mapping titles, calling <Product /> component

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ecommerce-shop-starter
+# This is a React JS, Tailwind CSS and Fake Store API call, Ecommerce Website.
--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -6,14 +6,21 @@ export const ProductContext = createContext();
 const ProductProvider = ({ children }) => {
   //products state
   const [products, setProducts] = useState([]);
-  //fetch products
+  // Fetch de productos
   useEffect(() => {
-    const fetchProducts =  async () => {
-      const response = await fetch('https://fakestoreapi.com/products');
-      const data = await response.json();
-      setProducts(data);
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching the products:', error);
+      }
     };
-  }, []);
+    // Invoca fetch
+    fetchProducts();
+  }, []); // array de dependencias vacío para que solo se ejecute una vez.
+
   
   return <ProductContext.Provider value={{products}}>{children}</ProductContext.Provider>;
 };
