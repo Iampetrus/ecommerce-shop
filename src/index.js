813af2e9ce81530da7d4527a2098c8c8be5b04fb
Iampetrus <pedroalerri@yahoo.es>
2024-09-30 17:01:30 +0200

    cartContext.js setup, added to index.js, Sidebar.js, Product.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 //product provider
-import ProductProvider from './contexts/ProductContext';
+import ProductProvider from "./contexts/ProductContext";
 //sidebar provider
-import SidebarProvider from './contexts/SidebarContext';
+import SidebarProvider from "./contexts/SidebarContext";
+//import cart provider
+import CartProvider from "./contexts/CartContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <SidebarProvider>
-    <ProductProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </ProductProvider>
+    <CartProvider>
+      <ProductProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ProductProvider>
+    </CartProvider>
   </SidebarProvider>
 );
